353692ecbca1ec34c8d5f92c1da97fed8f6c4e52
Kucman Mateusz <mateusz.kucman@igt.com>
2018-07-10 11:10:27 +0200

    Added inheritance module.

--- Inheritance/inherit.py
+++ Inheritance/inherit.py
@@ -2,5 +2,29 @@ class Animal(object):
     def __init__(self):
         print("Animal created")
 
-    def dog(self):
-        pass
+    def whoAmI(self):
+        print("Animal")
+
+    def eat(self):
+        print("Eating")
+
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog created")
+
+    def whoAmI(self):
+        # Any method that is the same as in the Parent class is overwritten by this.
+        print('Dog')
+
+    def bark(self):
+        print("woof!")
+
+
+a = Animal()
+print(a)
+
+d = Dog()
+d.eat()
+d.bark()
