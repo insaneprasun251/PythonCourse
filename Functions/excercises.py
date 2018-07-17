ed3ff953f0cf632dc203a1c02e145a2eccaf2ce8
Kucman Mateusz <mateusz.kucman@igt.com>
2018-07-17 17:04:55 +0200

    Added

--- Functions/excercises.py
+++ Functions/excercises.py
@@ -23,12 +23,48 @@ def make_twenty(a, b):
 
 
 def old_macdonald(name):
-    name2 = ''
-    for el in name:
-        if el == 0 or el == 3:
-            name2 += (name[el].upper())
-        name2 += name[el]
+    name2 = str(name[0].upper()) + str(name[1:3]) + str(name[3].upper()) + str(name[4:])
     return name2
 
 
-print(old_macdonald("macdonald"))
+def master_yoda(text):
+    a, b, c = text.split(" ")
+    text2 = " ".join([c, b, a])
+    return text2
+
+
+def almost_there(n):
+    if n in range(90, 111) or n in range(190, 211):
+        return True
+    return False
+
+
+def has_33(my_list):
+    if my_list[0] == my_list[1] == 3 or my_list[1] == my_list[2] == 3:
+        return True
+    return False
+
+
+def blackjack(a, b, c):
+    if sum([a, b, c]) <= 21:
+        return sum([a, b, c])
+    elif 11 in [a, b, c]:
+        return sum([a, b, c]) - 10
+    else:
+        return "BUST"
+
+
+def summer_69(arr):
+    # TODO: Correct This
+
+    result = 0
+    pass_until = 6
+    for i in arr:
+        if i != pass_until:
+            result += i
+        else:
+            if i != 9:
+                continue
+    return result
+
+
