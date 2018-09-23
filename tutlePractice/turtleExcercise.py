7c6f168ba837591fc4e6b441c40f6326c9ff7551
mkucman <mkucman@hotmail.com>
2018-09-23 21:43:27 +0200

    Added

--- tutlePractice/turtleExcercise.py
+++ tutlePractice/turtleExcercise.py
@@ -1,9 +1,9 @@
 
-from turtle import forward, left, done
-import turtle
+from turtle_game import forward, left, done
+import turtle_game
 forward(150)
 left(200)
-turtle.circle(100)
+turtle_game.circle(100)
 forward(400)
 
 done()
